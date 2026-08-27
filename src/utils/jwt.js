0220b09fff8d36acc639a0899d8a528de68448fc
JustBestDev <best.t.pimharn@gmail.com>
2026-08-28 00:17:13 +0700

    feat: implement login with JWT authentication and validation

---
+++ src/utils/jwt.js
@@ -0,0 +1,21 @@
+import jwt from 'jsonwebtoken'
+
+export const createTokenUser = (user) => {
+    const payload = {
+        id: user.id,
+        email: user.email,
+        username: user.username
+    }
+    const token = jwt.sign(payload, process.env.JWT_SECRET_USER, {
+        algorithm: 'HS256',
+        expiresIn: '1d'
+    })
+    return token
+}
+
+export const verifyToken = async (token) => {
+    const payload = jwt.verify(token, process.env.JWT_SECRET_USER, {
+        algorithms: ['HS256']
+    })
+    return payload
+}
