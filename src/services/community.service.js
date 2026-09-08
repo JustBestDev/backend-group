8bf3f463288cbc71ca034314d8e0467caafd5ba4
nantana-poppy <ppscc1003@gmail.com>
2026-09-08 19:03:50 +0700

    feat: include rooms in community post details

--- src/services/community.service.js
+++ src/services/community.service.js
@@ -58,6 +58,7 @@ export async function getAllCommunitiesService() {
           include: {
             images: true,
             address: true,
+            rooms: true,
           },
         },
         creator: {
